c428e02547289986848886e21c92726bb3335baa
Ross Golder <ross@golder.org>
2025-08-04 19:27:26 +0700

    fix: Add crossplane CLI dependency to xpkg publish target
    - Fixes GitHub Actions failure where crossplane CLI binary was missing
    - Ensures crossplane CLI is downloaded before attempting to push packages
    - Resolves 'No such file or directory' error in publish step

--- makelib/xpkg.mk
+++ makelib/xpkg.mk
@@ -97,7 +97,7 @@ $(foreach x,$(XPKGS),$(eval $(call xpkg.build.targets,$(x))))
 
 # 1: registry/org 2: repo
 define xpkg.release.targets
-xpkg.release.publish.$(1).$(2):
+xpkg.release.publish.$(1).$(2): $(CROSSPLANE_CLI)
 	@$(INFO) Pushing package $(1)/$(2):$(VERSION)
 	@$(CROSSPLANE_CLI) xpkg push \
 		$(foreach p,$(XPKG_LINUX_PLATFORMS),--package $(XPKG_OUTPUT_DIR)/$(p)/$(2)-$(VERSION).xpkg ) \
